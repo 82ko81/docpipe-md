555238211a61fec9e04349ac23b5bf2e91a95736
DNYS <139572243+82ko81@users.noreply.github.com>
2026-08-21 00:53:52 +0900

    feat: add format policy

---
+++ docpipe/policy.py
@@ -0,0 +1,4 @@
+"""Format routing constants shared by all conversion entry points."""
+
+UNHWP_EXTENSIONS = {".hwp", ".hwpx"}
+PANDOC_EXTENSIONS = {".docx", ".pptx", ".odt", ".rtf", ".html", ".htm"}
